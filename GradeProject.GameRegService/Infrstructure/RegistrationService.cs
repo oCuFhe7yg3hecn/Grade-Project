cb69948f0a96f35507fb00faa1f7100eb040a66c
Shalamai Artur <arturshalamai@gmail.com>
2018-06-25 15:59:14 +0300

    Added GameInfoModel

---
+++ GradeProject.GameRegService/Infrstructure/RegistrationService.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GradeProject.GameRegService.Infrstructure
+{
+    public class RegistrationService
+    {
+        private readonly HttpClient _httpClient;
+
+        public RegistrationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> RegisterGame(GameRegisterModel model)
+        {
+            var gameInfoResponse = await _httpClient.GetStringAsync(model.DocumentationUrl);
+            var gameInfo = JsonConvert.DeserializeAnonymousType<>(gameInfoResponse);
+
+            //Call RabitMQ to Catalog Service
+
+            //Call RabitMQ to Auth Service
+        }
+    }
+}
