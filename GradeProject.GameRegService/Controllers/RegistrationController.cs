2900f63950daf9d6b269e5005c37ef5efc1ac594
Shalamai Artur <arturshalamai@gmail.com>
2018-06-25 17:35:49 +0300

    Minor changes /dont matter/

--- GradeProject.GameRegService/Controllers/RegistrationController.cs
+++ GradeProject.GameRegService/Controllers/RegistrationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using GradeProject.GameRegService.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -19,11 +20,11 @@ namespace GradeProject.GameRegService.Controllers
         [Authorize(Roles = "developer")]
         public async Task<IActionResult> RegisterGame(string gameUrl)
         {
-            //var client = new HttpClient();
-            //var registerInfoendpoint = $"{gameUrl}/gradeproject-register-info";
-            //var response = await client.GetAsync(registerInfoendpoint);
+            var client = new HttpClient();
+            var registerInfoEndpoint = $"{gameUrl}/gradeproject-register-info";
+            var response = await client.GetStringAsync(registerInfoEndpoint);
 
-            //var response = JsonConvert.DeserializeObject<>(response);
+            //var responseObject = JsonConvert.DeserializeObject<GameInfo>(response);
 
             return Ok();
         }
