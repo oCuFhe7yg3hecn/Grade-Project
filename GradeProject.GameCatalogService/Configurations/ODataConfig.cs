ec37513c8038d2a6f9d921a715331851d06a2806
Shalamai Artur <arturshalamai@gmail.com>
2018-07-04 19:10:23 +0300

    Game Catalog Service Removed unused odata Model

--- GradeProject.GameCatalogService/Configurations/ODataConfig.cs
+++ GradeProject.GameCatalogService/Configurations/ODataConfig.cs
@@ -23,16 +23,6 @@ namespace GradeProject.GameCatalogService.Configurations
                         .Select() // Allow for the $select Command
                         .Expand();
 
-            builder.EntitySet<Category>("Categories")
-                        .EntityType
-                        .Filter() // Allow for the $filter Command
-                        .Count() // Allow for the $count Command
-                        .Expand() // Allow for the $expand Command
-                        .OrderBy() // Allow for the $orderby Command
-                        .Page() // Allow for the $top and $skip Commands
-                        .Select() // Allow for the $select Command
-                        .Expand();
-
             return builder;
         }
     }
