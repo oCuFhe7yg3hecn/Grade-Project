12f8ff35768ec88b368334b56751614576956577
Shalamai Artur <arturshalamai@gmail.com>
2018-07-04 19:26:10 +0300

    Game Profile Service Added Object Converter to byte array

---
+++ GradeProject.ProfileService/Utils/ObjectConverter.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeProject.ProfileService.Utils
+{
+    public static class ObjectConverter
+    {
+        public static byte[] ObjectToBytes(object obj)
+        {
+            if (obj != null)
+            {
+                var stringyfied = JsonConvert.SerializeObject(obj);
+                return Encoding.Default.GetBytes(stringyfied);
+            }
+            else
+            {
+                throw new Exception("Cannot serialize null");
+            }
+        }
+    }
+}
