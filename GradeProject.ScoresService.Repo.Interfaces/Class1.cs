eacaa759163e2842af4ea3d14de2c4f9f347b34d
Shalamai Artur <arturshalamai@gmail.com>
2018-07-12 12:08:30 +0300

    Added Generic Repo

---
+++ GradeProject.ScoresService.Repo.Interfaces/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GradeProject.ScoresService.Repo.Interfaces
+{
+    public class Class1
+    {
+    }
+}
---
+++ GradeProject.ScoresService.Repo.Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace GradeProject.ScoresService.Repo.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<int> CountAsync(Expression<Func<T, bool>> filter = null);
+
+        Task<T> SingleAsync(Expression<Func<T, bool>> filter);
+        Task<List<T>> WhereAsync(Expression<Func<T, bool>> filter, int count = 10, int page = 1);
+
+        Task AddOneAsync(T newGame);
+
+        Task<bool> UpdateOneAsync(Expression<Func<T, bool>> filter, T entiy);
+
+        Task<bool> DeleteOneAsync(Expression<Func<T, bool>> filter);
+
+        Task<bool> CheckASync(Expression<Func<T, bool>> statement);
+    }
+}
